49e0c0f67b348be08a15f0e67029c9d4782f8e6d
Prakhar Bindal <bindalprakhar@gmail.com>
2017-05-27 18:31:14 +0530

    all

---
+++ app/src/main/java/com/jigsaw/prakhar/menu_demo/MainActivity.java
@@ -0,0 +1,42 @@
+package com.jigsaw.prakhar.menu_demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_menu,menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        super.onOptionsItemSelected(item);
+
+        switch (item.getItemId()){
+            case (R.id.settings):
+                Log.i("settings tapped","settings");
+                return true;
+            case (R.id.help):
+                Log.i("help tapped","help");
+                return true;
+            default:
+                return false;
+
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
